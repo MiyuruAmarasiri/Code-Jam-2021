2179f9d105734ce743aae148b92a1f974f8c6eca
Miyuru Bhashitha Amarasiri <60309527+Miyuru-Bhashitha-amarasiri@users.noreply.github.com>
2021-04-26 00:29:03 +0530

    Round 1B

---
+++ Digit Blocks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+}
